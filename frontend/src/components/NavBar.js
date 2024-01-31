55bee857032ff7080099262793663189d2b42403
neeru0713 <neerurani1307@gmail.com>
2024-01-31 06:28:03 +0530

    bellicon notification functionality completed

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -2,28 +2,61 @@ import React, { useState, useEffect, useRef, useContext } from "react";
 import { CgSearch } from "react-icons/cg";
 import Register from "./Register";
 import { Link } from "react-router-dom";
+import { SlBell } from "react-icons/sl";
 import { UserContext } from "../App";
+
 const NavBar = () => {
   const [searchText, setSearchText] = useState("");
   const [showRegisterModal, setShowRegisterModal] = useState(false);
-  const [isPopoverVisible, setPopoverVisible] = useState(false);
+    const [isPopoverVisible, setPopoverVisible] = useState(false);
+  const [isClickedBell, setIsClickedBell] = useState(false);
+  const [notification, setNotification] = useState([])
   const { user } = useContext(UserContext);
   const popoverRef = useRef(null);
-
+const popoverRef1 = useRef(null);
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (popoverRef.current && !popoverRef.current.contains(event.target)) {
         setPopoverVisible(false);
       }
-    };
 
-    document.addEventListener("mousedown", handleClickOutside);
+       if (popoverRef1.current && !popoverRef1.current.contains(event.target)) {
+         setIsClickedBell(false);
+      }
+      
+      };
+      const handleClickOutside1 = (event) => {
+        if (popoverRef1.current && !popoverRef1.current.contains(event.target)) {
+          setIsClickedBell(false);
+        }
+      };
+
+      document.addEventListener("mousedown", handleClickOutside);
 
     return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
+        document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://api.example.com/data");
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        setNotification(result);
+        
+      } catch (error) {
+        console.error("Error fetching data:", error.message);
+       
+      }
+    };
+
+    fetchData();
+  }, []);
   const handlePopoverToggle = () => {
     setPopoverVisible(!isPopoverVisible);
   };
@@ -34,8 +67,11 @@ const NavBar = () => {
 
   const openModal = () => {
     setShowRegisterModal(true);
-  };
-
+    };
+    
+    const handleClickBell = () => {
+      setIsClickedBell(!isClickedBell);  
+    }
   const handleSearchChange = (e) => {
     setSearchText(e.target.value);
   };
@@ -63,6 +99,27 @@ const NavBar = () => {
         </div>
 
         <div className="flex justify-between gap-6 items-center">
+          <div className="relative">
+            <SlBell
+              className="text-lg cursor-pointer"
+              onClick={handleClickBell}
+              ref={popoverRef1}
+            />
+            {isClickedBell && (
+              <div className="bell-popover rounded-lg absolute bg-white shadow-md top-[45px] left-[-30px]">
+                <ul className="p-4 border border-gray-300 rounded-lg border-lg w-[150px] text-center flex flex-col">
+                  {notification.map((notification, index) => (
+                    <li
+                      key={index}
+                      className="rounded-lg p-1 hover:bg-blue-100"
+                    >
+                      {notification.title}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </div>
           <Link to="/createevent">
             <button className="font-medium border border-gray-400 px-3 py-1 rounded-lg text-white bg-blue-800 hover:bg-blue-600">
               Create Event
@@ -81,6 +138,7 @@ const NavBar = () => {
             <span>Hi, {user?.name || "Guest"}</span>
             {isPopoverVisible && (
               <div className="popover rounded-lg absolute bg-white right-0 shadow-md top-10">
+                {/* Content of the user icon popover */}
                 <ul className="p-4 border border-gray-300 rounded-lg border-lg w-[150px] text-center flex flex-col">
                   {!user && (
                     <li
@@ -95,7 +153,6 @@ const NavBar = () => {
                       Manage Events
                     </li>
                   )}
-
                   <li className="rounded-lg p-1 hover:bg-blue-100">
                     My Bookings
                   </li>
